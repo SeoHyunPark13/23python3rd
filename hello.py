4966e2579931aca424ed909eccdec9f45d3f8944
SeoHyunPark13 <144295584+SeoHyunPark13@users.noreply.github.com>
2023-12-04 19:03:15 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
-print("Hello World")
+print("Hello World")    # "Hello World"를 출력하는 print 함수 호출
 Hello World
 "Hello World"
 'Hello World'
